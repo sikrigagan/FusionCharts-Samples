996b466e0c435fa881aea52c9d83f37dc1029b5f
Gagan Sikri <sikrigagan@gmail.com>
2016-06-12 16:12:01 +0530

    Add Angular Samples - Basic & Advanced

---
+++ Angular/pydio.js
@@ -0,0 +1,47 @@
+FusionCharts.register('theme', {
+    name: 'pydio',
+    theme: {
+        base: {
+            chart: {
+                "captionFontSize": "30",
+                "subCaptionFontSize": "20",
+                "captionFontBold": "0",
+                "baseFont": "Open Sans",
+                "baseFontSize": "14",
+                "xAxisNameFontBold": "0",
+                "yAxisNameFontBold": "0",
+                "xAxisNameFontSize": "16",
+                "yAxisNameFontSize": "16",
+                "plotSpacePercent": "16",
+                "bgColor": "#50585F",
+                "bgAlpha": "100",
+                "canvasBgAlpha": "0",
+                "canvasBgColor": "#50585F",
+                "baseFontColor": "#FCFDFD",
+                "captionPadding": "20",
+                "toolTipBgColor": "#000000",
+                "toolTipPadding": "13",
+                "toolTipBgAlpha": "70",
+                "toolTipBorderColor": "#EEE555",
+                "toolTipBorderThickness": "1",
+                "toolTipBorderRadius": "3",
+                "ToolTipBorderAlpha": "30",
+                "usePlotGradientColor": "0",
+                "showAlternateHGridColor": "0",
+                "use3DLighting": "0",
+                "smartLineColor": "#FCFDFD",
+                "smartLineThickness": "1.5",
+                "doughnutRadius": "65",
+                "centerLabel": "Revenue from $label: $Value",
+                "numberPrefix": "$",
+                "showPlotBorder": "0",
+                "canvasBorderAlpha": "0",
+                "showXaxisLine": "1",
+                "xAxisLineColor": "#FCFDFD",
+                "xAxisLineAlpha": "40"
+        	}
+            
+        }
+        
+    }
+});
